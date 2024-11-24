9bca374168b56a524b921ff760b8708c4b7e63ae
Bjathneas <bjathneas@gmail.com>
2024-11-24 02:49:15 -0600

    retry

--- html/search/all_a.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['library_0',['bLib - Little b&apos;s Library',['../index.html',1,'']]],
-  ['little_20b_20s_20library_1',['bLib - Little b&apos;s Library',['../index.html',1,'']]],
-  ['log_2',['log',['../namespacebLib.html#a62a9e9b26863f954b346a121d42da738',1,'bLib']]],
-  ['log_5flevel_3',['LOG_LEVEL',['../namespacebLib.html#ab0b4da750afe41ec1c292a4540a6a5d1',1,'bLib']]],
-  ['logger_2eh_4',['Logger.h',['../Logger_8h.html',1,'']]]
-];
--- html/search/pages_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['library_0',['bLib - Little b&apos;s Library',['../index.html',1,'']]],
-  ['little_20b_20s_20library_1',['bLib - Little b&apos;s Library',['../index.html',1,'']]]
-];
